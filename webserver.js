33d209b515893663b650be141231794d0b5d60f1
jwalter <joakim.waltersson@gmail.com>
2011-06-25 23:04:29 +0000

    Added web page scraping to fetch the menus

--- webserver.js
+++ webserver.js
@@ -1,9 +1,31 @@
 require("jade");
-var app = require("express").createServer();
+var express = require('express');
+var scraper = require("scraper");
+var app = module.exports = express.createServer();
 
-app.set("view engine", "jade");
+app.configure(function(){
+   app.set('views', __dirname + '/views');
+   app.set('view engine', 'jade');
+   app.use(express.bodyParser());
+   app.use(express.methodOverride());
+   app.use(require('stylus').middleware({ src: __dirname + '/public' }));
+   app.use(app.router);
+   app.use(express.static(__dirname + '/public'));
+});
 app.get("/", function(req, res) {
     res.render("index");
+});
+
+app.get("/lunch", function(req, res) {
+    scraper("http://du-o-ja.se/", function(err, jQuery) {
+        if (err) {throw err;}
+        var menu = "";
+        jQuery("p").first().contents().filter("h5").wrap("<h5></h5>").end().
+        each(function() {
+            menu += jQuery(this).text().trim() + "<BR>";
+        });
+        res.send(menu);
+    });
 });
 
 app.listen(process.env.C9_PORT);
